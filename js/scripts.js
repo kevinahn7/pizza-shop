6d6e81832bb22775baedf18efd2f77f56f2e9a37
Kevin Ahn <kevinahn7@hotmail.com>
2018-06-15 11:23:01 -0700

    Fix phrasing of toppings to include spaces, commas and an and

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function Pizza(size, toppings) {
     this.size = size;
     this.toppings = toppings;
@@ -36,7 +37,25 @@ Pizza.prototype.getPrice = function() {
     return totalPrice;
 };
 
+Pizza.prototype.getToppingsPhrase = function() {
+    if (this.toppings.length > 1) {
+        var firstToppings = this.toppings.splice(0, this.toppings.length - 1);
+        var lastTopping = this.toppings.pop();
+        var firstToppingsBeforeAnd = ""
+        var firstToppingsRest = firstToppings.splice(0, firstToppings.length - 1);
+        var firstToppingsLastOneBeforeAnd = firstToppings.pop();
+        firstToppingsRest.forEach(topping => {
+            firstToppingsBeforeAnd += topping + ", ";
+        });
+        var phrase = firstToppingsBeforeAnd + firstToppingsLastOneBeforeAnd + " and " + lastTopping;
+        return phrase;
+    } 
+    else {
+        return this.toppings;
+    }
+}
 
+//User Interface Logic
 $(document).ready(function() {
     $(".makeOrder").submit(function(event) {
         event.preventDefault();
@@ -47,7 +66,8 @@ $(document).ready(function() {
             toppingsOfPizza.push(individualToppings);
           });
         var pizzaOrder = new Pizza(sizeofPizza, toppingsOfPizza)
-
         var finalPrice = pizzaOrder.getPrice();
+        var toppingsPhrase = pizzaOrder.getToppingsPhrase();
+        $(".results").text("Your " + pizzaOrder.size + " pizza with " + toppingsPhrase + " will be $" + finalPrice + " dollars total.");
     })
 });
